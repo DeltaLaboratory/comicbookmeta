537ad2347214533b22bf42cc80914380d279bb9d
DeltaLaboratory <delta@deltalab.dev>
2022-09-23 23:17:11 +0900

    Version 0.1.1
    - Feature
      - Removed Publisher, Distributor Attributes, Alternative is authors.publisher and author.distributor
      - Added publisher, distributor, original_author and original_illustrator Roles in authors
    - Bug
      - Fixed Title Attribute in Golang Module
    - Improvements
      - Added omitempty Flag to Golang Module

--- README.md
+++ README.md
@@ -4,5 +4,5 @@ Comic Book Metadata(CBM) is a schema for comic book archive.
 ## format
 CBM is a json file which located at root of comic book archive, and named `cbm.json`.
 ## schema
-current schema version is `0.1.0`.
+current schema version is `0.1.1`.
 schema file can be found at [here](cbm.schema.json).
--- cbm.go
+++ cbm.go
@@ -4,25 +4,21 @@ import "time"
 
 type CBM struct {
 	// The title of the comic book
-	Title string
+	Title string `json:"title"`
 	// The identifiers of the comic book
 	Identifiers []Identifier `json:"identifiers"`
 	// The authors of the comic book
-	Authors []Author `json:"authors"`
+	Authors []Author `json:"authors,omitempty"`
 	// "The series data of the comic book"
-	Series *Series `json:"series"`
+	Series *Series `json:"series,omitempty"`
 	// The tags of the comic book
-	Tags []string `json:"tags"`
+	Tags []string `json:"tags,omitempty"`
 	// The language of the comic book in ISO 639-1 format
-	Language *string `json:"language"`
-	// The publisher of the comic book
-	Publisher *string `json:"publisher"`
-	// The distributor of the comic book
-	Distributor *string `json:"distributor"`
+	Language *string `json:"language,omitempty"`
 	// The date of publication of the comic book
-	PublishedDate *time.Time `json:"published_date"`
+	PublishedDate *time.Time `json:"published_date,omitempty"`
 	// The date of distribution of the comic book
-	DistributedDate *time.Time `json:"distributed_date"`
+	DistributedDate *time.Time `json:"distributed_date,omitempty"`
 	// The read direction of the comic book
 	ReadDirection string `json:"read_direction"`
 }
@@ -36,22 +32,22 @@ type Identifier struct {
 
 type Author struct {
 	// The identifier of the author
-	Identifier string `json:"identifier"`
+	Identifier string `json:"identifier,omitempty"`
 	// The name of the author
 	Name string `json:"name"`
 	// The role of the author
-	Role string `json:"role"`
+	Role string `json:"role,omitempty"`
 }
 
 type Series struct {
 	// The identifier of the series
-	Identifier string `json:"identifier"`
+	Identifier string `json:"identifier,omitempty"`
 	// The title of the series
 	Title string `json:"name"`
 	// The number of the comic book in the series
 	Index int `json:"index"`
 	// The parent series data of the comic book
-	Parent *SeriesParent `json:"parent"`
+	Parent *SeriesParent `json:"parent,omitempty"`
 }
 
 type SeriesParent struct {
